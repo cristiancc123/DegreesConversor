533a21e9cf81cdef3c3cd6065f23fcefa1342d1e
cristian <cristian@PC>
2017-08-13 21:21:12 -0500

    Creación del Proyecto y Carpetas MVVM

---
+++ DegreesConversor/DegreesConversor/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,15 @@
+﻿namespace DegreesConversor.Infrastructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+
+        public MainViewModels Main { get; set; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModels();
+        }
+
+    }
+}
